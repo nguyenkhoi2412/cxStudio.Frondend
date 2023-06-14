c006120a1bd2f301d420681abc3182e661ec8b0f
Khoi Nguyen <62054711+nguyenkhoi2412@users.noreply.github.com>
2023-06-14 19:41:57 +0700

    .

--- src/views/dashboard/default/default.js
+++ src/views/dashboard/default/default.js
@@ -1,14 +1,20 @@
 import { hookInstance } from "@utils/hookInstance";
+import { useTheme, styled } from "@mui/material/styles";
 import DashboardMainContentCardWrapper from "@dashboard/_layout/mainContentCardWrapper";
 import SecondaryAction from "@components/mui-ui/cards/cardSecondaryAction";
 
 const DashboardDefault = (props) => {
+  const theme = useTheme();
+
   return (
     <DashboardMainContentCardWrapper
       title={`Dashboard default`}
-      secondary={
-        <SecondaryAction link="https://mui.com/material-ui/material-icons/" />
-      }
+      // secondary={
+      //   <SecondaryAction
+      //     link="https://mui.com/material-ui/material-icons/"
+      //     color={theme.palette.background.default}
+      //   />
+      // }
     ></DashboardMainContentCardWrapper>
   );
 };
