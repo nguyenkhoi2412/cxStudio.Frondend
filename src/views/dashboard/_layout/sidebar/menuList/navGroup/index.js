4fd145f4bf1267193e21c1fa8f67fc5c1a479707
Khoi Nguyen <62054711+nguyenkhoi2412@users.noreply.github.com>
2023-06-15 09:24:04 +0700

    .

--- src/views/dashboard/_layout/sidebar/menuList/navGroup/index.js
+++ src/views/dashboard/_layout/sidebar/menuList/navGroup/index.js
@@ -34,10 +34,10 @@ const NavGroup = ({ item }) => {
             <List
                 subheader={
                     item.title && (
-                        <Typography variant="caption" sx={{ ...theme.typography.menuCaption }} display="block" gutterBottom>
+                        <Typography variant="caption" className="MuiTypography-menuCaption" display="block" gutterBottom>
                             {item.title}
                             {item.caption && (
-                                <Typography variant="caption" sx={{ ...theme.typography.subMenuCaption }} display="block" gutterBottom>
+                                <Typography variant="caption" className="MuiTypography-subMenuCaption" display="block" gutterBottom>
                                     {item.caption}
                                 </Typography>
                             )}
