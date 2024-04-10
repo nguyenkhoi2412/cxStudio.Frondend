0a0602090d670d89bf37b6725de4b4ff173d14b3
Khoi Nguyen <62054711+nguyenkhoi2412@users.noreply.github.com>
2024-04-10 15:39:43 +0700

    .

--- src/utils/crossCutting.js
+++ src/utils/crossCutting.js
@@ -1628,6 +1628,42 @@ export const hook = {
   },
 
   /*
+   * useAsync
+   * How to use it?
+   * const imgFetch = useAsync(url =>
+   *  fetch(url).then(response => response.json())
+   * );
+   * use: imgFetch.loading, imgFetch.error, imgFetch.data
+   */
+  useAsync: (fn) => {
+    const initialState = { loading: false, error: null, data: null };
+    const stateReducer = (_, action) => {
+      switch (action.type) {
+        case 'start':
+          return { loading: true, error: null, data: null };
+        case 'finish':
+          return { loading: false, error: null, data: action.data };
+        case 'error':
+          return { loading: false, error: action.error, data: null };
+      }
+    };
+
+    const [state, dispatch] = React.useReducer(stateReducer, initialState);
+
+    const run = async (args = null) => {
+      try {
+        dispatch({ type: 'start' });
+        const value = await fn(args);
+        dispatch({ type: 'finish', data });
+      } catch (error) {
+        dispatch({ type: 'error', error });
+      }
+    };
+
+    return { ...state, run };
+  },
+
+  /*
    * useWindowSize
    * Call hook when window resize
    * const [width, height] = useWindowSize();
